aec8e74831028a7489b5f791aa883bda27a8401b
Adarsh Pandey <ap272003@gmail.com>
2025-11-23 18:29:53 +0530

    Local host updated to Internet Ip

--- Website/run_app.py
+++ Website/run_app.py
@@ -2,4 +2,12 @@
 from app import app
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    host = os.environ.get('HOST', '0.0.0.0')
+    try:
+        port = int(os.environ.get('PORT', 5000))
+    except (TypeError, ValueError):
+        port = 5000
+    debug = os.environ.get('FLASK_DEBUG', 'False').lower() in ('1', 'true', 'yes')
+
+    app.run(host=host, port=port, debug=debug)
